82b8240696fdab0ff3a4cd6106aad9be9fce4443
snega07 <snegapalanisamy@gmail.com>
2022-12-13 19:35:40 +0530

    notes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
+import Header from './component/Header';
 function App() {
+  const [data,setData] = useState({title:'',notes:''});
+  const [store,setStore]= useState([]);
+  const handleChange=(e)=>{
+    setData({...data,[e.target.name]:e.target.value})
+  }
+  const handleDelete=(ind)=>{
+    var newstore= store;
+     newstore.splice(ind,1);
+     setStore([...newstore]);
+     console.log(store);
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const newstore = store;
+    newstore.unshift({notes:data.notes,title:data.title})
+    setStore([...newstore]);
+    console.log(store);
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Header/>
+      <div className="App">
+        <input  style={{width:'200px',height:'30px'}}name='title'  placeholder='Title'onChange={handleChange}></input><br/>
+      <textarea style={{width:'200px',height:'200px'}} name='notes' placeholder='Take a note' onChange={handleChange}/> <br/>
+      <button type='submit' id ='add' onClick={handleSubmit}>add</button>
+  
     </div>
+    {store.map((store,ind)=>{
+          return(
+        <div key={ind} className='res' style={{ width:'200px', height:'200px'}}>
+          <div style={{paddingLeft:'10px'}}>
+          <br/> <b> {store.title}</b><br/>
+        <p>  {store.notes}</p>
+          
+          <button type='submit' id='delete' onClick={()=>handleDelete(ind)}>delete</button>
+          </div>
+        </div>
+          )
+        })}
+           </div>
+   
   );
 }
 
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import './style.css'
+function Header(){
+return(
+    <div style={{height:'60px',width:'100%',backgroundColor:'orange'}}>
+<p id='headtext'>Keeper</p>
+    </div>
+)
+}
+export default Header;
